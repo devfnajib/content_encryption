dbc122d0778aa5ae4f3eab2abb8d3a9ea25f40ab
devfnajib <choudharyfahad@gmail.com>
2024-11-28 22:15:04 -0500

    Logging improved

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
+from loguru import logger
 
 from configs import DB_CONNECTION_STRING
 from app.models import db
@@ -10,13 +11,17 @@ from app.routes_main import bp as main_bp
 
 
 def create_application():
+    logger.info('Creating Flask App.')
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTION_STRING
     db.init_app(app)
     migrate = Migrate(app, db)
+    logger.success('DB connection setup.')
+
     app.register_blueprint(protection_systems_bp)
     app.register_blueprint(devices_bp)
     app.register_blueprint(contents_bp)
     app.register_blueprint(main_bp)
+    logger.success('All routes registered.')
 
     return app
--- app/routes_devices.py
+++ app/routes_devices.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request, jsonify
+from loguru import logger
+import shortuuid
 
 from app.models import db, Device, ProtectionSystem
 
@@ -8,6 +10,8 @@ bp = Blueprint('devices', __name__)
 
 @bp.route('/devices', methods=['POST'])
 def create_device():
+    request_id = shortuuid.uuid()
+    logger.info(f'[ReqID: "{request_id}"]: POST Request received at "/devices"')
     request_payload = request.get_json()
     name = request_payload.get('name', None)
     protection_system_id = request_payload.get('protection_system_id', None)
@@ -21,13 +25,16 @@ def create_device():
         error_message = '"protection_system_id" is required field.'
 
     if protection_system_id is not None:
+        logger.info(f'[ReqID: "{request_id}"]: Validating protection_system_id.')
         protection_system_id = int(protection_system_id)
         try:
             ProtectionSystem.query.get_or_404(protection_system_id)
+            logger.info(f'[ReqID: "{request_id}"]: protection_system_id validated.')
         except Exception as ex:
             error_message = 'No Protection System found for "protection_system_id"={}.'.format(protection_system_id)
 
     if error_message is not None:
+        logger.error(f'[ReqID: "{request_id}"]: {error_message}')
         data = {
             'status': 'Error',
             'message': error_message
@@ -35,6 +42,7 @@ def create_device():
         response = (jsonify(data), 400)
         return response
 
+    logger.info(f'[ReqID: "{request_id}"]: Creating new Device.')
     new_device = Device(name=name, protection_system=protection_system_id)
     db.session.add(new_device)
     db.session.commit()
@@ -44,11 +52,14 @@ def create_device():
         'message': 'Device created successfully.'
     }
     response = (jsonify(data), 201)
+    logger.success(f'[ReqID: "{request_id}"]: Device created successfully.')
     return response
 
 
 @bp.route('/devices/<int:id>', methods=['GET'])
 def get_device(id):
+    request_id = shortuuid.uuid()
+    logger.info(f'[ReqID: "{request_id}"]: GET Request received at "/devices/{id}"')
     device = Device.query.get_or_404(id)
     data = {
         'id': device.id,
@@ -57,11 +68,14 @@ def get_device(id):
     }
 
     response = (jsonify(data), 200)
+    logger.success(f'[ReqID: "{request_id}"]: Returning Device details.')
     return response
 
 
 @bp.route('/devices', methods=['GET'])
 def get_devices():
+    request_id = shortuuid.uuid()
+    logger.info(f'[ReqID: "{request_id}"]: GET Request received at "/devices"')
     devices = Device.query.all()
     data = []
     for device in devices:
@@ -74,11 +88,14 @@ def get_devices():
         data.append(device_data)
 
     response = (jsonify(data), 200)
+    logger.success(f'[ReqID: "{request_id}"]: Returning all Devices.')
     return response
 
 
 @bp.route('/devices/<int:id>', methods=['PUT'])
 def update_device(id):
+    request_id = shortuuid.uuid()
+    logger.info(f'[ReqID: "{request_id}"]: PUT Request received at "/devices/{id}"')
     device = Device.query.get_or_404(id)
 
     request_payload = request.get_json()
@@ -90,13 +107,16 @@ def update_device(id):
         error_message = 'At least one of the following parameters is reqwuired to update: "name" and "protection_system_id"'
 
     if protection_system_id is not None:
+        logger.info(f'[ReqID: "{request_id}"]: Validating protection_system_id.')
         protection_system_id = int(protection_system_id)
         try:
             ProtectionSystem.query.get_or_404(protection_system_id)
+            logger.info(f'[ReqID: "{request_id}"]: protection_system_id validated.')
         except Exception as ex:
             error_message = 'No Protection System found for "protection_system_id"={}.'.format(protection_system_id)
 
     if error_message is not None:
+        logger.error(f'[ReqID: "{request_id}"]: {error_message}.')
         data = {
             'status': 'Error',
             'message': error_message
@@ -104,6 +124,7 @@ def update_device(id):
         response = (jsonify(data), 400)
         return response
 
+    logger.info(f'[ReqID: "{request_id}"]: Updating device.')
     if name is not None:
         device.name = name
     if protection_system_id is not None:
@@ -115,11 +136,14 @@ def update_device(id):
         'message': 'Device updated successfully.'
     }
     response = (jsonify(data), 200)
+    logger.success(f'[ReqID: "{request_id}"]: Device updated successfully.')
     return response
 
 
 @bp.route('/devices/<int:id>', methods=['DELETE'])
 def delete_device(id):
+    request_id = shortuuid.uuid()
+    logger.info(f'[ReqID: "{request_id}"]: DELETE Request received at "/devices/{id}"')
     device = Device.query.get_or_404(id)
     db.session.delete(device)
     db.session.commit()
@@ -128,4 +152,5 @@ def delete_device(id):
         'message': 'Device deleted successfully.'
     }
     response = (jsonify(data), 200)
+    logger.success(f'[ReqID: "{request_id}"]: Device deleted successfully.')
     return response
